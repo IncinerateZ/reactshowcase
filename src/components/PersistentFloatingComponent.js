fd21d3a3ba2533c66182257039071b28562cb36b
IncinerateZ <inferious77@gmail.com>
2022-01-20 21:00:30 +0700

    github link

--- src/components/PersistentFloatingComponent.js
+++ src/components/PersistentFloatingComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import 'App.css';
+import githubLogo from 'imgs/GitHub-Mark-Light-120px-plus.png';
 
 export default function PersistenFloatingComponent() {
     var rxo = useSelector((state) => state.reduxone);
@@ -14,10 +15,21 @@ export default function PersistenFloatingComponent() {
                 border: '1px solid #111',
             }}
         >
-            <div className='floater__icon' style={{fontSize: '2rem'}}>!</div>
+            <div className='floater__icon' style={{ fontSize: '2rem' }}>
+                !
+            </div>
             <div className='floater__content'>
                 Persistent Floating Component{' '}
                 <p>Redux Stored Value: {rxo.value}</p>
+                <div>
+                    <a href='https://github.com/IncinerateZ/reactshowcase'>
+                        <img
+                            src={githubLogo}
+                            alt='github'
+                            style={{ width: '35px', cursor: 'pointer' }}
+                        ></img>
+                    </a>
+                </div>
             </div>
         </div>
     );
